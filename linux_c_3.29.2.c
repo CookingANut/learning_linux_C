b9bff78ce98aaf5fe29977a35fa26b0e2910c1c2
L <morningrocks@outlook.com>
2023-11-29 10:09:18 +0000

    Add comments explaining the usage of opendir, readdir, and closedir functions

--- linux_c_3.29.2.c
+++ linux_c_3.29.2.c
@@ -8,6 +8,23 @@
 #define MAX_PATH 1024
 
 /* dirwalk: apply fcn to all files in dir */
+
+/*
+opendir(3)/readdir(3)/closedir(3)用于遍历目录数据块中的记录。
+opendir打开一个目录，返回一个DIR *指针代表这个目录，它是一个类似FILE *指针的句柄，
+closedir用于关闭这个句柄，把DIR*指针传给
+readdir读取目录数据块中的记录，每次返回一个指向struct dirent的指针，反复读就可以遍历所有记录，所有记录遍历完之后readdir返回NULL。
+
+结构体struct dirent的定义如下：
+struct dirent 
+{ 
+    ino_t d_ino;                //inode number 
+    off_t d_off;                //offset to the next dirent 
+    unsigned short d_reclen;    //length of this record 
+    unsigned char d_type;       //type of file  
+    char d_name[256];           //filename 
+};
+*/
 void dirwalk(char *dir, void (*fcn)(char *)) {
     char name[MAX_PATH];
     struct dirent *dp;
